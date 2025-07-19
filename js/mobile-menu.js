3f1ccfb77876f7b6cea08bc900075df312ebe952
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-07-19 18:56:47 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".t-l-navigation-list");
-  const headerRef = document.querySelector(".t-l-header");
-  const navigationRef = document.querySelector(".t-l-navigation");
+  const navListRef = document.querySelector(".t-b-navigation-list");
+  const headerRef = document.querySelector(".t-b-header");
+  const navigationRef = document.querySelector(".t-b-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".t-l-navigation-link").forEach((n) =>
+  document.querySelectorAll(".t-b-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
--- js/slick.js
+++ js/slick.js
@@ -1,15 +1,1 @@
-$(function () {
-  $(".t-l-reviews-list").slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    centerMode: true,
-    cssEase: "linear",
-    prevArrow: ".t-l-reviews-list-left",
-    nextArrow: ".t-l-reviews-list-right",
-  });
-});
+
